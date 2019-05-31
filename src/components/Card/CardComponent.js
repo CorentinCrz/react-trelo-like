62bcc9326e1372d837d3bc3efba04451800fe374
CorentinCrz <ccroiz@live.fr>
2019-05-31 19:21:10 +0200

    edit card

--- src/components/Card/CardComponent.js
+++ src/components/Card/CardComponent.js
@@ -5,11 +5,17 @@ const CardComponent = props => {
     const handleChangeName = (event) => {
         props.editCard(props.listIndex, props.cardIndex, 'name', event.target.value)
     }
+    const handleChangeDescription = (event) => {
+        props.editCard(props.listIndex, props.cardIndex, 'descritpion', event.target.value)
+    }
 
     return (
         <section className="over">
             <form className="card">
-                <input onChange={handleChangeName} value={props.card.name} autoFocus/>
+                <label for="title">Titre</label>
+                <input onChange={handleChangeName} value={props.card.name} id="title"/>
+                <label for="descritpion">Descritpion</label>
+                <textarea onChange={handleChangeDescription} value={props.card.descritpion} id="descritpion"/>
                 <button onClick={() => props.editModal(null, null)} className="close">X</button>
             </form>
         </section>
